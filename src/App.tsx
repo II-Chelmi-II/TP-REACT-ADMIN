a2830b42dfc69b458d008d67ade9f879941c21f9
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-04-28 17:33:49 +0300

    feat: Add feature to edit user info

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import {
   ShowGuesser,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { UserList } from "./UserList";
 
 export const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name="users" list={ListGuesser} recordRepresentation={"name"} />
+  <Resource name="users" list={UserList} edit={EditGuesser} recordRepresentation={"name"} />
   <Resource name="posts" list={ListGuesser} />
   <Resource name="comments" list={ListGuesser} />
 </Admin>;
