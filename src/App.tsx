a63b249164dd4da5e0d6d352530129d58023064b
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-04-28 17:27:34 +0300

    chore: Configure App.tsx for better User display

--- src/App.tsx
+++ src/App.tsx
@@ -7,4 +7,8 @@ import {
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+export const App = () => <Admin dataProvider={dataProvider}>
+  <Resource name="users" list={ListGuesser} recordRepresentation={"name"} />
+  <Resource name="posts" list={ListGuesser} />
+  <Resource name="comments" list={ListGuesser} />
+</Admin>;
